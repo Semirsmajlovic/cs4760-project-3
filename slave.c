1d79f22fe68811b8a431d689700a1a16e0cad3fb
Semir Smajlovic <semir.smajlovic@fleishman.com>
2022-03-14 23:12:05 -0500

    Include out slave include for execution

---
+++ slave.c
@@ -0,0 +1,13 @@
+// Define our includes
+#include <signal.h>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
