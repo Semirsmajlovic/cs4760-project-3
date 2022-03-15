0afafea80cd6155932491702634e87c8a3806445
Semir Smajlovic <semir.smajlovic@fleishman.com>
2022-03-14 23:06:51 -0500

    Add our defined includes for project

---
+++ master.c
@@ -0,0 +1,16 @@
+// Define our includes
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
